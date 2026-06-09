678c70c9ec9c4a2a01f49ee9c6cafa44f64d7fba
ivansk1 <ivanski.isabelli@escola.pr.gov.br>
2026-06-09 14:38:43 +0000

    dia 2

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-from flask import Flask, render_template app Flask()@app.route('/') def inicio(): return render_template('index.html') app.run(host-'0.0.0.0', port -300)
+from flask import Flask, render_template 
+
+app = Flask(_name_)
+
+@app.route('/')
+def inicio():
+        nome = "Turma de Programação"
+        curso = "Python = HTML"
+
+        return render _template(
+            'index.html',
+            nome = nome,
+            curso = curso
+        )
+
+@app.route('/sobre')        
+def sobre():
+    return """
+    <h1>Sobre a Turma</h1>
+    <p>Esse projeto foi criado usando Python e HTML.</p>
+    <a href="/">Voltar para o início</a>
+    """
+
+if _name_ =='_main_':
+    app.run(host='0.0.0.0', port=3000, debug=True)
